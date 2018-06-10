fd89fa21ade3d0be71e8f0057f21918c7457956b
DB <dwblair@localhost.localdomain>
2018-06-10 15:07:53 -0400

    update

--- lorarx.py
+++ lorarx.py
@@ -8,9 +8,14 @@ import adafruit_rfm9x
 #rfm9x = adafruit_rfm9x.RFM9x(spi, cs, reset, 915.0)
 rfm9x = adafruit_rfm9x.RFM9x(spi, cs, reset, 868.0)
 #rfm9x.send('Hello world!')
-while True:
-    packet = rfm9x.receive()
-    packet_text = str(packet, 'ascii')
-    print('Received (ASCII): {0}'.format(packet_text))
-    rssi = rfm9x.rssi
-    print('Received signal strength: {0} dB'.format(rssi))
+
+def listen():
+    while True:
+        packet = rfm9x.receive(timeout=5.0)
+        if packet is None:
+            print("nada ...")
+        else:
+            packet_text = str(packet, 'ascii')
+            print('Received (ASCII): {0}'.format(packet_text))
+            rssi = rfm9x.rssi
+            print('Received signal strength: {0} dB'.format(rssi))
