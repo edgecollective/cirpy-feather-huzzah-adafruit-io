fe1b7d634812e3eff3114e04e756ab3014aec69e
DB <dwblair@localhost.localdomain>
2018-06-10 15:51:36 -0400

    update

---
+++ lorarx_adapost.py
@@ -0,0 +1,37 @@
+import board
+import busio
+import digitalio
+import urequests
+
+spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
+cs = digitalio.DigitalInOut(board.GPIO15)
+reset = digitalio.DigitalInOut(board.GPIO2)
+import adafruit_rfm9x
+#rfm9x = adafruit_rfm9x.RFM9x(spi, cs, reset, 915.0)
+rfm9x = adafruit_rfm9x.RFM9x(spi, cs, reset, 868.0)
+#rfm9x.send('Hello world!')
+
+headers={'Content-Type': 'application/json','X-AIO-Key':'3515b3ecee734780927d7f4ab1654917'}
+url='https://io.adafruit.com/api/v2/donblair/feeds/goof-test/data.json'
+#json=dict(foo='13.2')
+#json=dict(foo='13.2)
+
+def post(json):
+    r=urequests.post(url,json=json,headers=headers)
+    return r
+    
+def listen():
+    while True:
+        packet = rfm9x.receive(timeout=5.0)
+        if packet is None:
+            print("nada ...")
+        else:
+            packet_text = str(packet, 'ascii').strip()
+            print('Received (ASCII): {0}'.format(packet_text))
+            rssi = rfm9x.rssi
+            print('Received signal strength: {0} dB'.format(rssi))
+            print('sending to adafruit.io ...')
+            #datum = float(packet_text)
+            json=dict(value=packet_text)
+            post(json)
+            
